d592ca89d6b9def1487905e71c084b6c440ef7e4
ataricoder <ataricoder@gmail.com>
2019-02-11 18:19:19 -0600

    added new view in storyboard and new view controller. initialized code to print out movie title in console

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -43,7 +43,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 
                 //call all functions below again
                 self.tableView.reloadData()
-                print(dataDictionary)
+                //print(dataDictionary)
                 
                 // TODO: Get the array of movies
                 // TODO: Store the movies in a property to use elsewhere
@@ -89,14 +89,23 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        //Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //Pass the selected movie into the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
 
 }
