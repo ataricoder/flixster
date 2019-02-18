55f359f5a1a27416291f670862aa3bcef2a5dbba
ataricoder <ataricoder@gmail.com>
2019-02-17 23:22:11 -0600

    added tab bar config, and superhero view controller. added api to that vc and data for images to appear on ui

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by ataricoder on 2/17/19.
+//  Copyright © 2019 danielmunoz. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
